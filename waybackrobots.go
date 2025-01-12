5494986db48648da3a77bdd6e3207cf058d827aa
Oskar Wulff <oskar9899@gmail.com>
2025-01-12 23:08:06 +0100

    addded args

--- waybackrobots.go
+++ waybackrobots.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
 	"net/http"
@@ -13,10 +14,26 @@ import (
 type WaybackTimestamps [][]string
 
 func main() {
-	host := "www.toyota-europe.com"
+	var domain string
+	var fromDate int
+	flag.StringVar(&domain, "domain", "", "which domain to find old robots for")
+	flag.IntVar(&fromDate, "fd", 2015, "choose date from when to get robots from format: 2015")
+	flag.Parse()
+
+	if domain == "" {
+		fmt.Fprint(os.Stderr, "supply a domain")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	if len(strings.Split(domain, ".")) < 1 {
+		fmt.Fprintf(os.Stderr, "ERROR not a valid domain: %s\n", domain)
+		os.Exit(1)
+	}
+
 	var wt WaybackTimestamps
 
-	timestamps := fmt.Sprintf("https://web.archive.org/cdx/search/cdx?url=%s/robots.txt&output=json&fl=timestamp,original&filter=statuscode:200&collapse=digest&from=2012", host)
+	timestamps := fmt.Sprintf("https://web.archive.org/cdx/search/cdx?url=%s/robots.txt&output=json&fl=timestamp,original&filter=statuscode:200&collapse=digest&from=%d", domain, fromDate)
 	resp, err := http.Get(timestamps)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "ERROR getting timestamps response: %v\n", err)
@@ -43,7 +60,7 @@ func main() {
 	var uniquePaths []string
 	for _, e := range wt {
 		ts := e[0]
-		snapshotUrl := fmt.Sprintf("https://web.archive.org/web/%sid_/%s/robots.txt", ts, host)
+		snapshotUrl := fmt.Sprintf("https://web.archive.org/web/%sid_/%s/robots.txt", ts, domain)
 		resp, err := http.Get(snapshotUrl)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "ERROR getting robots resp: %v\n", err)
@@ -65,7 +82,7 @@ func main() {
 			for _, tc := range strings.Split(textContent, "\n") {
 				if strings.Contains(tc, "Disallow:") {
 					path := strings.Split(tc, "Disallow:")
-					if slices.Contains(uniquePaths, path[1]) && path[1] != " " {
+					if !slices.Contains(uniquePaths, path[1]) && path[1] != "" {
 						uniquePaths = append(uniquePaths, path[1])
 						fmt.Println(path[1])
 					}
