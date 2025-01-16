40e1f80a0f0c66f1e8c76b2c5b937c1baf39350a
Oskar Wulff <oskar9899@gmail.com>
2025-01-16 22:40:23 +0100

    update

--- waybackrobots.go
+++ waybackrobots.go
@@ -17,7 +17,7 @@ import (
 type WaybackTimestamps [][]string
 
 func getRobotsContent(domain, ts string) []string {
-	fmt.Fprintf(os.Stderr, "[DEBUG] downloading %v\n", ts)
+	// fmt.Fprintf(os.Stderr, "[DEBUG] downloading %v\n", ts)
 
 	snapshotUrl := fmt.Sprintf("https://web.archive.org/web/%sid_/%s/robots.txt", ts, domain)
 	resp, err := http.Get(snapshotUrl)
@@ -58,12 +58,19 @@ func getRobotsContent(domain, ts string) []string {
 	return nil
 }
 
-func getTimestamps(domain string, fromDate int) {
+func getTimestamps(domain, strategy string, fromDate int) {
 
 	var wt WaybackTimestamps
-
-	// timestamps := fmt.Sprintf("https://web.archive.org/cdx/search/cdx?url=%s/robots.txt&output=json&fl=timestamp,original&filter=statuscode:200&collapse=digest&from=%d", domain, fromDate)
-	timestamps := fmt.Sprintf("https://web.archive.org/cdx/search/cdx?url=%s/robots.txt&output=json&fl=timestamp,original&filter=statuscode:200&collapse=timestamp:7&from=%d", domain, fromDate)
+	var timestamps string
+	if strategy == "digest" {
+		timestamps = fmt.Sprintf("https://web.archive.org/cdx/search/cdx?url=%s/robots.txt&output=json&fl=timestamp,original&filter=statuscode:200&collapse=digest&from=%d", domain, fromDate)
+	} else if strategy == "day" {
+		timestamps = fmt.Sprintf("https://web.archive.org/cdx/search/cdx?url=%s/robots.txt&output=json&fl=timestamp,original&filter=statuscode:200&collapse=timestamp:7&from=%d", domain, fromDate)
+	} else if strategy == "month" {
+		timestamps = fmt.Sprintf("https://web.archive.org/cdx/search/cdx?url=%s/robots.txt&output=json&fl=timestamp,original&filter=statuscode:200&collapse=timestamp:6&from=%d", domain, fromDate)
+	} else {
+		return
+	}
 	resp, err := http.Get(timestamps)
 	if err != nil {
 		color.Set(color.FgRed)
@@ -118,27 +125,37 @@ func main() {
 
 	var domain string
 	var fromDate int
+	var strategy string
+	strategyValues := []string{"digest", "day", "month"}
 
 	flag.StringVar(&domain, "domain", "", "which domain to find old robots for")
+	flag.StringVar(&strategy, "strat", "digest", "interval to get robots for, possible values: digest, day, month")
 	flag.IntVar(&fromDate, "fd", 2015, "choose date from when to get robots from format: 2015")
 
 	flag.Parse()
+
+	if !slices.Contains(strategyValues, strategy) {
+		color.Set(color.FgRed)
+		fmt.Fprintf(os.Stderr, "%s is not a valid value for strat flag\n", strategy)
+		color.Unset()
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 
 	if domain != "" {
 		if len(strings.Split(domain, ".")) < 1 {
 			fmt.Fprintf(os.Stderr, "ERROR not a valid domain: %s\n", domain)
 			os.Exit(1)
 		}
-		getTimestamps(domain, fromDate)
+		getTimestamps(domain, strategy, fromDate)
 	} else {
 		scanner := bufio.NewScanner(os.Stdin)
 		for scanner.Scan() {
-
 			domain := scanner.Text()
 			if len(strings.Split(domain, ".")) < 1 {
 				fmt.Fprintf(os.Stderr, "Skipping %s no a valid domain\n", domain)
 			} else {
-				getTimestamps(domain, fromDate)
+				getTimestamps(domain, strategy, fromDate)
 			}
 		}
 	}
